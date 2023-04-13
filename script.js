658be380d6bed1525e53e0786d9c4cce5c3b4379
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-04-13 16:48:57 +0200

    update

--- script.js
+++ script.js
@@ -1,23 +1,108 @@
 const taskInput = document.getElementById('taskInput');
-        const taskList = document.getElementById('taskList');
+const taskList = document.getElementById('taskList');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskCounter = document.getElementById('taskCounter');
+const filterButtons = document.getElementById('filterButtons');
+const filterAll = document.getElementById('filterAll');
+const filterActive = document.getElementById('filterActive');
+const filterCompleted = document.getElementById('filterCompleted');
 
-        taskInput.addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') {
-                addTask(event.target.value);
-                event.target.value = '';
-            }
+let currentFilter = 'all';
+
+taskInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        addTask(event.target.value);
+        event.target.value = '';
+    }
+});
+
+addTaskBtn.addEventListener('click', () => {
+    addTask(taskInput.value);
+    taskInput.value = '';
+});
+
+filterAll.addEventListener('click', () => {
+    currentFilter = 'all';
+    updateFilterButtons();
+    filterTasks();
+});
+
+filterActive.addEventListener('click', () => {
+    currentFilter = 'active';
+    updateFilterButtons();
+    filterTasks();
+});
+
+filterCompleted.addEventListener('click', () => {
+    currentFilter = 'completed';
+    updateFilterButtons();
+    filterTasks();
+});
+
+function addTask(task) {
+    if (task.trim() !== '') {
+        const listItem = document.createElement('li');
+
+        const taskText = document.createElement('span');
+        taskText.textContent = task;
+        listItem.appendChild(taskText);
+
+        const completeBtn = document.createElement('button');
+        completeBtn.textContent = '✓';
+        completeBtn.classList.add('complete-btn');
+        completeBtn.addEventListener('click', () => {
+            taskText.classList.toggle('completed');
+            updateTaskCounter();
         });
+        listItem.appendChild(completeBtn);
 
-        function addTask(task) {
-            if (task.trim() !== '') {
-                const listItem = document.createElement('li');
-                listItem.textContent = task;
-                listItem.addEventListener('click', () => {
-                    listItem.classList.toggle('completed');
-                });
-                listItem.addEventListener('dblclick', () => {
-                    taskList.removeChild(listItem);
-                });
-                taskList.appendChild(listItem);
-            }
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'X';
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.addEventListener('click', () => {
+            taskList.removeChild(listItem);
+            updateTaskCounter();
+        });
+        listItem.appendChild(deleteBtn);
+
+        taskList.appendChild(listItem);
+        updateTaskCounter();
+    }
+}
+
+function updateTaskCounter() {
+    const activeTasks = document.querySelectorAll('#taskList li:not(.completed)').length;
+    taskCounter.textContent = `${activeTasks} tâches en cours`;
+}
+
+function updateFilterButtons() {
+    filterAll.classList.toggle('active', currentFilter === 'all');
+    filterActive.classList.toggle('active', currentFilter === 'active');
+    filterCompleted.classList.toggle('active', currentFilter === 'completed');
+    filterTasks();
+}
+
+function filterTasks() {
+    const tasks = taskList.getElementsByTagName('li');
+    for (let task of tasks) {
+        switch (currentFilter) {
+            case 'all':
+                task.style.display = '';
+                break;
+            case 'active':
+                if (task.querySelector('.completed')) {
+                    task.style.display = 'none';
+                } else {
+                    task.style.display = '';
+                }
+                break;
+            case 'completed':
+                if (task.querySelector('.completed')) {
+                    task.style.display = '';
+                } else {
+                    task.style.display = 'none';
+                }
+                break;
         }
+    }
+}
